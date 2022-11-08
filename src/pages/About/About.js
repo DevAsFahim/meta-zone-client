129cf0b34f9f2c80076ce3126ba023d8b56a8eef
ASFahim007 <devasfahim@gmail.com>
2022-11-08 11:18:11 +0600

    about section added

---
+++ src/pages/About/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import aboutImg from '../../assets/image/about.png'
+
+const About = () => {
+    return (
+        <div className='container mx-auto py-20'>
+            <div className="hero min-h-screen">
+                <div className="hero-content flex-col lg:flex-row gap-8">
+                    <img src={aboutImg} className="w-3/5 rounded-lg shadow-4xl p-4 border" alt=''/>
+                    <div>
+                        <small className='text-orange-500'>HAVE A LOOK ABOUT ME</small>
+                        <h1 className="text-5xl font-bold text-zinc-200 mt-6">About</h1>
+                        <p className="py-6"> I am Jone Lee.I have more than 6 years experience in Adobe Photoshop,Adobe Illustrator and Adobe In design.I am expert in clipping path,background remove,transparent background,photo editing,Image retouch,Image manipulation,Image masking.I am here to carry out your activity. Apart from this i also run my own business by providing online services to international customers like Photo editing, Adobe Photoshop, Adobe Lightroom, color grading, etc.</p>
+                        <p className="py-6">Apart from this i also run my own business by providing online services to international customers like Photo editing, Adobe Photoshop, Adobe Lightroom, color grading, graphic designer, etc.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import About from '../About/About';
 import './Home.css'
 
 const Home = () => {
     return (
-        <div className="hero min-h-screen place-content-start ">
-            <div className='pt-28 pl-10'>
-                <div className="w-1/2">
-                    <h1 className="mb-5 text-5xl font-bold">Hi, I’m <span className='text-orange-500'>Jone Lee</span></h1>
-                    <h1 className="mb-5 text-5xl font-bold">a Wedding Photography Expert</h1>
-                    <p className="mb-5">I use high quality Photography camera to take picture. Having the best lens for wedding photography will help you capture those special moments, no matter where they occur. Lenses come in all shapes</p>
+        <>
+            <div className="hero banner min-h-screen place-content-start ">
+                <div className='pt-28 pl-10'>
+                    <div className="w-1/2">
+                        <h1 className="mb-5 text-5xl font-bold">Hi, I’m <span className='text-orange-500'>Jone Lee</span></h1>
+                        <h1 className="mb-5 text-5xl font-bold">a Wedding Photography Expert</h1>
+                        <p className="mb-5">I use high quality Photography camera to take picture. Having the best lens for wedding photography will help you capture those special moments, no matter where they occur. Lenses come in all shapes</p>
+                    </div>
                 </div>
             </div>
-        </div>
+            <About></About>
+        </>
     );
 };
 
